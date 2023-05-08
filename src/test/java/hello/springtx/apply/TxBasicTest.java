9ddae8385675e14cc0af30d2cbc9f86b26bca48d
drssee <sarmos88@naver.com>
2023-05-08 23:59:27 +0900

    23/05/08

---
+++ src/test/java/hello/springtx/apply/TxBasicTest.java
@@ -0,0 +1,63 @@
+package hello.springtx.apply;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+import static org.assertj.core.api.Assertions.*;
+
+@Slf4j
+@SpringBootTest
+public class TxBasicTest {
+
+    @Autowired
+    BasicService basicService;
+
+    @Test
+    void proxyCheck() {
+        log.info("aop class={}", basicService.getClass());
+        assertThat(AopUtils.isAopProxy(basicService)).isTrue();
+    }
+
+    @Test
+    void txTest() {
+        basicService.tx();
+        basicService.nonTx();
+    }
+
+    @TestConfiguration
+    static class TxApplyBasicConfig {
+        @Bean
+        BasicService basicService() {
+            //빈에 등록하려고 보니깐 @Transactional이 붙어있네?
+            //프록시 적용 객체를 빈으로 대신 등록하자
+            return new BasicService();
+        }
+    }
+
+    @Slf4j
+    static class BasicService {
+
+        @Transactional
+        public void tx() {
+            log.info("call tx");
+            //실행중인 쓰레드가 트렌젝션 중이냐?(ThreadLocal)
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active={}", txActive);
+        }
+
+        public void nonTx() {
+            log.info("call NonTx");
+            //실행중인 쓰레드가 트렌젝션 중이냐?
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active={}", txActive);
+        }
+    }
+}
