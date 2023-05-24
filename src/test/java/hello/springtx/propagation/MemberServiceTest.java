b11cc7afe2ccb4fce97b3cc36a25ce1e520b0873
drssee <sarmos88@naver.com>
2023-05-24 20:56:27 +0900

    23/05/24

--- src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.repository.query.Param;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -35,5 +36,84 @@ class MemberServiceTest {
         //then
         Assertions.assertTrue(memberRepository.find(username).isPresent());
         Assertions.assertTrue(logRepository.find(username).isPresent());
+    }
+
+    /**
+     * memberService @Transactional:OFF
+     * memberRepository @Transactional:ON
+     * logRepository @Transactional:ON Exception
+     */
+    @Test
+    void outerTxOff_fail() {
+        //given
+        String username = "로그예외_outerTxOff_fail";
+
+        //when
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            memberService.joinV1(username);
+        });
+
+        //then
+        Assertions.assertTrue(memberRepository.find(username).isPresent());
+        Assertions.assertTrue(logRepository.find(username).isEmpty());
+    }
+
+    /**
+     * memberService @Transactional:ON
+     */
+    @Test
+    void singleTx() {
+        //given
+        String username = "outerTxOff_success";
+
+        //when
+        memberService.joinV1(username);
+
+        //then
+        Assertions.assertTrue(memberRepository.find(username).isPresent());
+        Assertions.assertTrue(logRepository.find(username).isPresent());
+    }
+
+    /**
+     * memberService @Transactional:ON
+     * memberRepository @Transactional:ON
+     * logRepository @Transactional:ON
+     */
+    @Test
+    void outerTxOn_Success() {
+        //given
+        String username = "outerTxOn_Success";
+
+        //when
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            memberService.joinV1(username);
+        });
+
+        //then
+        Assertions.assertTrue(memberRepository.find(username).isPresent());
+        Assertions.assertTrue(logRepository.find(username).isEmpty());
+    }
+
+    /**
+     * memberService @Transactional:ON
+     * memberRepository @Transactional:ON
+     * logRepository @Transactional:ON Exception
+     */
+    //기본전파로 존재하는 트랜잭션에 참여해도
+    //그 트랜잭션의 aop코드는 존재함 단지 물리 커넥션을 가진게 아니라(신규 트랜잭션x),
+    //상위 트랜잭션에 결과를 마킹만함
+    @Test
+    void outerTxOn_fail() {
+        //given
+        String username = "로그예외_outerTxOn_fail";
+
+        //when
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            memberService.joinV1(username);
+        });
+
+        //then
+        Assertions.assertTrue(memberRepository.find(username).isEmpty());
+        Assertions.assertTrue(logRepository.find(username).isEmpty());
     }
 }
