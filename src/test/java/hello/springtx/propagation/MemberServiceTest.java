69601cdd6dfe017ade63685e530a55f16cf57e16
drssee <sarmos88@naver.com>
2023-05-25 22:49:44 +0900

    23/05/25

--- src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -5,9 +5,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.UnexpectedRollbackException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * 대원칙
+ * 같은 물리 트랜잭션 내의 논리 트랜잭션들이 모두 논리커밋 되어야 물리커밋이 된다
+ * 신규 트랜잭션이 물리 트랜잭션이다(물리 커넥션을 가진다)
+ */
 @SpringBootTest
 class MemberServiceTest {
 
@@ -114,6 +120,56 @@ class MemberServiceTest {
 
         //then
         Assertions.assertTrue(memberRepository.find(username).isEmpty());
+        Assertions.assertTrue(logRepository.find(username).isEmpty());
+    }
+
+    /**
+     * memberService @Transactional:ON
+     * memberRepository @Transactional:ON
+     * logRepository @Transactional:ON Exception
+     */
+    @Test
+    void recoverException_fail() {
+        /*
+        참여한 내부 트랜잭션에서 런타임예외가 발생해 롤백 -> 하지만 내부 참여 트랜잭션이라 롤백only만 마크함
+        던져진 예외가 서비스까지 올라간다 -> 하지만 서비스예서 예외를 잡아서 처리 후 정상적으로 aop에게 흐름이 넘어감
+        aop는 외부 트랜잭션이 정상종료 되었으니 커밋을 요청함 -> 하지만 이미 트랜잭션 롤백only가 마크되어 롤백됨
+        그리고 외부 트랜잭션 입장에서는 커밋을 요청했지만 롤백이 발생한 경우니 기대하지않은롤백 예외가 터진다
+         */
+
+        //given
+        String username = "로그예외_recoverException_fail";
+
+        //when
+        memberService.joinV2(username);
+
+        //then
+        Assertions.assertTrue(memberRepository.find(username).isEmpty());
+        Assertions.assertTrue(logRepository.find(username).isEmpty());
+    }
+
+    /**
+     * memberService @Transactional:ON
+     * memberRepository @Transactional:ON
+     * logRepository @Transactional:ON(REQUIRES_NEW) Exception
+     */
+    @Test
+    void recoverException_success() {
+        /*
+        참여한 내부 트랜잭션에서 런타임예외가 발생해 롤백 -> 하지만 내부 참여 트랜잭션이라 롤백only만 마크함
+        던져진 예외가 서비스까지 올라간다 -> 하지만 서비스예서 예외를 잡아서 처리 후 정상적으로 aop에게 흐름이 넘어감
+        aop는 외부 트랜잭션이 정상종료 되었으니 커밋을 요청함 -> 하지만 이미 트랜잭션 롤백only가 마크되어 롤백됨
+        그리고 외부 트랜잭션 입장에서는 커밋을 요청했지만 롤백이 발생한 경우니 기대하지않은롤백 예외가 터진다
+         */
+
+        //given
+        String username = "로그예외_recoverException_success";
+
+        //when
+        memberService.joinV2(username);
+
+        //then
+        Assertions.assertTrue(memberRepository.find(username).isPresent());
         Assertions.assertTrue(logRepository.find(username).isEmpty());
     }
 }
